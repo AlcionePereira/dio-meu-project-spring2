5a865c9f82b21ca30458f40aa277122076d33469
Alcione Pereira <catce.2021211mtds0453@gmail.com.ifpi.edu>
2022-07-25 02:36:46 -0300

    Finaliando o Bootcamp Santander às 02:35, com spring

---
+++ meu-project-spring2/src/main/java/br/com/meuPrimeiroProjeto/meuprojectspring2/MeuProjectSpring2Application.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojectspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjectSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjectSpring2Application.class, args);
+	}
+
+}
---
+++ meu-project-spring2/src/test/java/br/com/meuPrimeiroProjeto/meuprojectspring2/MeuProjectSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojectspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjectSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
